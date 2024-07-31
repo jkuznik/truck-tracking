a550c14147813bfad41764291f48cb7ef5333b05
jkuznik <janusz.kuznik89@gmail.com>
2024-07-31 15:26:50 +0200

    Connect local repository with remote, add necessary and additional dependencies

---
+++ src/main/java/pl/jkuznik/trucktracking/TruckTrackingApplication.java
@@ -0,0 +1,13 @@
+package pl.jkuznik.trucktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jkuznik/trucktracking/TruckTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jkuznik.trucktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
